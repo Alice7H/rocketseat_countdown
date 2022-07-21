fcbfb2eb0e2cda2f61d6a2c793f2f08c1d5e667d
Alice Hata <alicehata9@gmail.com>
2022-07-21 00:19:04 -0300

    update style,form.js and index.html

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -9,6 +9,10 @@
       alert(`Nome ${name.value} e e-mail ${email.value}`);
       name.value = '';
       email.value = '';
+    } else if (name.value === '') {
+      alert('O campo de nome não foi preenchido.');
+    } else if (email.value === '') {
+      alert('O campo de e-mail não foi preenchido.');
     }
   })
 })()
